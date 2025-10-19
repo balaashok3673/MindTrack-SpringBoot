9b7fd790c6d30e284d9de71b828ed29a4e75ce11
Bala Kumar <balaashok2004@gmail.com>
2025-10-19 12:36:46 +0530

    added repository layer

--- src/main/java/com/bala/MindTracker/repository/TaskRepository.java
+++ src/main/java/com/bala/MindTracker/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.bala.MindTracker.repository;
 
-public class TaskRepository {
+import com.bala.MindTracker.model.TaskModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskModel , Long> {
 }
