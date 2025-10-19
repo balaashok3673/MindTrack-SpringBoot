dc468240907ae2edb2426a7e09e7c04641bbfcfa
Bala Kumar <balaashok2004@gmail.com>
2025-10-19 12:02:51 +0530

    Intial commit

---
+++ src/test/java/com/bala/MindTracker/MindTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.MindTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
